60bad6159398f9c2408c54abbf26fc5adaca02e7
Luis Lechuga <luislechu55@gmail.com>
2022-07-07 20:31:50 -0500

    feat: ejercicio 3

--- api/controllers/restaurantController.js
+++ api/controllers/restaurantController.js
@@ -15,6 +15,38 @@ const getAllRestaurants = async (_, res) => {
   }
 };
 
+const getFiveRestaurants = async (_, res) => {
+  try {
+    const restaurants = await Restaurant.find().limit(5);
+    return res.json({
+      msg: '5 Restaurantes obtenidos',
+      restaurants,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: 'Error al buscar los Restaurantes',
+      error,
+    });
+  }
+};
+
+const getSpecificRestaurants = async (_, res) => {
+  try {
+    const restaurants = await Restaurant.find().skip(5).limit(5);
+    return res.json({
+      msg: 'Restaurantes 6 a 10 obtenidos',
+      restaurants,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: 'Error al encontrar los Restaurantes',
+      error,
+    });
+  }
+};
+
 export {
   getAllRestaurants,
+  getFiveRestaurants,
+  getSpecificRestaurants
 }
--- api/routes/restaurantRoutes.js
+++ api/routes/restaurantRoutes.js
@@ -3,8 +3,10 @@ import { restaurantController } from '../controllers/index.js';
 
 const router = express.Router();
 
-router
-.route('/restaurants')
-.get(restaurantController.getAllRestaurants);
+router.get('/restaurants', restaurantController.getAllRestaurants);
+
+router.get('/fiveRestaurants', restaurantController.getFiveRestaurants);
+
+router.get('/especificRestaurants', restaurantController.getSpecificRestaurants);
 
 export default router;
